32c03c87858cd247b64c67a0fb1078be9b0a14b1
chenys1995 <argorok7@gmail.com>
2015-11-01 14:32:59 +0800

    this is begin file.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	
+
+
+
+
+
+	return 0;
+}
+
